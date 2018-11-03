a6a7e08c03eb4b98673fa6f5038dcb1c9878ef23
DESKTOP-065S021\chjdt98 <2902647122@qq.com>
2018-11-03 17:25:42 +0800

    20181103 c++作业 415 杜利雄

---
+++ du.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   double sales;
+   double wage;
+   cout << "Enter sales in dollars (-1 to end): ";
+   cin >> sales;
+
+   while ( a != -1.0 )
+   {
+      wage = 200.0 + 0.09 * sales;
+      cout << "Salary is: $" << wage;
+      cout << "\n\nEnter sales in dollars (-1 to end): ";
+      cin >> sales;
+   }
+   return0
+}
