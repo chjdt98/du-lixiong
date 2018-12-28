f916d2ea7e257c4fa51eff0aabf7910a2b7bd9a3
DESKTOP-065S021\chjdt98 <2902647122@qq.com>
2018-12-28 22:35:10 +0800

    20181228 c++作业 杜利雄

---
+++ 9.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Time.h"
+using namespace std;
+
+int main()
+{
+   Time t;
+
+   cout << "The universal time is ";
+   t.printUniversal();
+   cout << "\nThe standard time is ";
+   t.printStandard();
+   cout << endl;
+} // end main
+
+
